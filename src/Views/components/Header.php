a1ec7a759645ad50d8dc8bed1ee5fa6b07616d4d
todo-list <dinhthanhtien2k2@gmail.com>
2023-03-19 23:25:57 +0700

    Card UI

---
+++ src/Views/components/Header.php
@@ -0,0 +1,23 @@
+<?php
+    function Headers() { ?>
+        <div class="header" >
+            <div class="colorThumbail" style="background-color: rgb(248, 104, 16)"></div>
+            <div class="colorShadowThumbail"></div>
+            <div></div>
+            <div class="thumbail">
+                <img src="https://i.scdn.co/image/ab67616d00001e02be0ac2aa3ed0047463210db1" alt="" />
+            </div>
+            <div class="content">
+                <span class="title">Album</span>
+                <span class="nameContent"><h1>LINK</h1></span>
+                <div class="description">
+                    <div>
+                        <img src="https://i.scdn.co/image/ab6761610000f178be8b6591753659ca595c73c6" alt="" />
+                        <span><a href="/">Hoang Thuy Linh</a></span>
+                    </div>
+                    <span class="dot">2022</span>
+                    <span class="dot">9 songs, <span>30 min 32 sec</span></span>
+                </div>
+            </div>
+        </div>
+    <?php } ?>
